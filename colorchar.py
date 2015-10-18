430cc47b2a6661a683ef560903cfe73988c943bd
Richard Barnes <rbarnes@umn.edu>
2015-10-18 14:33:51 -0700

    Initial import

---
+++ colorchar.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import argparse
+
+char_color      = dict()
+char_color['1'] = '\033[35m'
+char_color['3'] = '\033[33m'
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', '-i', type = argparse.FileType('r'), default = '-')
+args = parser.parse_args()
+
+for line in args.input:
+  for k,v in char_color.items():
+    line = line.replace(k,v+k)
+  print(line, end="")
+
+
+print("\033[39m")
