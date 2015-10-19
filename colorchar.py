ff03faab2b29063f0f1f1847253bc75da2750a4e
Richard Barnes <rbarnes@umn.edu>
2015-10-18 21:17:25 -0700

    Added a word boundary option

--- colorchar.py
+++ colorchar.py
@@ -7,16 +7,21 @@ import re
 reset_color     = "\033[39m"
 char_color      = dict()
 char_color['1'] = '\033[35m'
+char_color['2'] = '\033[34m'
 char_color['3'] = '\033[33m'
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', '-i', type = argparse.FileType('r'), default = '-')
+parser.add_argument('--input',    '-i', type = argparse.FileType('r'), default = '-')
+parser.add_argument('--boundary', '-b', action="store_const", const=True, default=False)
 args = parser.parse_args()
 
 for line in args.input:
   for k,v in char_color.items():
-    line = re.sub(r'(?<=\b)'+k+r'(?=\b)',v+k+reset_color,line)
+    if args.boundary:
+      line = re.sub(r'(?<=\b)'+k+r'(?=\b)',v+k+reset_color,line)
+    else:
+      line = re.sub(k,v+k+reset_color,line)
   print(line, end="")
 
 print(reset_color)
